c0d15513dbe04b94f73bb7d0c2d40ad5a6787df8
alexgab <alexgabriel3357@gmail.com>
2023-12-08 16:43:18 -0300

    Correção de responsividade
    Co-Authored-By: Lucas Spinosa <lucasspinosa.software@gmail.com>

--- alinhaSequencia.js
+++ alinhaSequencia.js
@@ -192,17 +192,17 @@ function imprimirAoContrario(str) {
 function estilizaString(resultadoHorizontal, resultadoVertical){
    for(let i = 0; i < resultadoHorizontal.length; i++){
       if(resultadoHorizontal[i] == resultadoVertical[i]){
-         htmlHorizontal += `<span class=no-weigth strings>${resultadoHorizontal[i]}</span>` 
-         htmlVertical += `<span class=no-weigth strings>${resultadoVertical[i]}</span>` 
+         htmlHorizontal += `<span class=no-weigth strings>${resultadoHorizontal[i]} </span>` 
+         htmlVertical += `<span class=no-weigth strings>${resultadoVertical[i]} </span>` 
       }
       else{
          if(resultadoHorizontal[i] == '-' || resultadoVertical[i] == '-'){
-            htmlHorizontal += `<span class=gap strings>${resultadoHorizontal[i]}</span>` 
-            htmlVertical += `<span class=gap strings >${resultadoVertical[i]}</span>` 
+            htmlHorizontal += `<span class=gap strings>${resultadoHorizontal[i]} </span>` 
+            htmlVertical += `<span class=gap strings >${resultadoVertical[i]} </span>` 
          }
          else{
-            htmlHorizontal += `<span class=weigth strings>${resultadoHorizontal[i]}</span>` 
-            htmlVertical += `<span class=weigth strings>${resultadoVertical[i]}</span>` 
+            htmlHorizontal += `<span class=weigth strings>${resultadoHorizontal[i]} </span>` 
+            htmlVertical += `<span class=weigth strings>${resultadoVertical[i]} </span>` 
          }
          }
    }
